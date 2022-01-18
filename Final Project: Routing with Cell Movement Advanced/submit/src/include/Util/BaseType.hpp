4dc9b50bc849f30c64359b8b9d9c67541e060a89
Koyama-Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2022-01-18 12:55:41 +0800

    Delete Final Project: Routing with Cell Movement Advanced/submit/src directory

--- Final Project: Routing with Cell Movement Advanced/submit/src/include/Util/BaseType.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <ostream>
-namespace Util {
-class Outputable {
-public:
-  virtual void to_ostream(std::ostream &out) const = 0;
-};
-} // namespace Util
--- Final Project: Routing with Cell Movement Advanced/submit/src/lib/cell_move_router/Input/Raw/Datas.d
+++
@@ -1,3 +0,0 @@
-lib/cell_move_router/Input/Raw/Datas.o: \
- lib/cell_move_router/Input/Raw/Datas.cpp \
- include/cell_move_router/Input/Raw/Datas.hpp include/Util/BaseType.hpp
